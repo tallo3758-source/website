e041ad9718fdee6ba1d515abe79d8cefd0a08bfb
TLo <tallo3758@gmail.com>
2026-08-01 21:00:06 -0400

    mobile2

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const video = document.getElementById('myVideo')
 const isMobile = window.matchMedia("(pointer: coarse)").matches || window.innerWidth <= 768;
 
 if (isMobile) {
-    setTimeout(spawnHeartFromVideo, i * 40);
+    setTimeout(spawnHeartFromVideo, 30 * 40);
 }
 video.addEventListener('play', (e) => {
     for (let i = 0; i < 20; i++) {
