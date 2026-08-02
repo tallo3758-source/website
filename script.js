51b9a55ed13b2be5711a9736d5563312eafeb240
TLo <tallo3758@gmail.com>
2026-08-01 20:55:51 -0400

    mobile1

--- script.js
+++ script.js
@@ -1,44 +1,52 @@
 const heart = document.getElementById('heart');
 const video = document.getElementById('myVideo')
+const isMobile = window.matchMedia("(pointer: coarse)").matches || window.innerWidth <= 768;
 
+if (isMobile) {
+    setTimeout(spawnHeartFromVideo, i * 40);
+}
 video.addEventListener('play', (e) => {
-    burstHearts(20);
+    for (let i = 0; i < 20; i++) {
+        setTimeout(spawnHeartFromVideo, i * 40); // Staggered release
+    }
 });
 function spawnHeartFromVideo() {
     const videoElement = document.getElementById('myVideo');
     
-    // Get position of video directly
+    // Get position of video and center
     const rect = videoElement.getBoundingClientRect();
     const centerX = rect.left + rect.width / 2;
     const centerY = rect.top + rect.height / 2;
 
+    //create a heart element
     const heart = document.createElement('div');
     heart.classList.add('arc-heart');
 
+    /*this is just css
+        .heart{
+            left: centerXpx;
+        }
+    */
     heart.style.left = `${centerX}px`;
     heart.style.top = `${centerY}px`;
 
-    // Random direction and distance straight outward
+    // Random directoin and distance
     const angle = Math.random() * Math.PI * 2;
     const distance = 200 + Math.random() * 150; 
 
     const xEnd = Math.cos(angle) * distance;
     const yEnd = Math.sin(angle) * distance;
 
-    // Set end points and rotation
+    // Set end points and rotation also pretty much css
     heart.style.setProperty('--x-end', `${xEnd}px`);
     heart.style.setProperty('--y-end', `${yEnd}px`);
     heart.style.setProperty('--rot', `${Math.random() * 360}deg`);
 
-    // Append to body so screen-relative coordinates align perfectly
+    // Append to body
     document.body.appendChild(heart);
+
 
     heart.addEventListener('animationend', () => {
         heart.remove();
     });
-}
-function burstHearts(count = 15) {
-    for (let i = 0; i < count; i++) {
-        setTimeout(spawnHeartFromVideo, i * 40); // Staggered release
-    }
 }
